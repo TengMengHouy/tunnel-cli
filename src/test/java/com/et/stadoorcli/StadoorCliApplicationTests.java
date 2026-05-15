416501ba46a07364a928fe9562c0eb1d0ef13266
TengMengHouy <la0989975@gmail.com>
2026-05-15 08:32:48 +0700

    first commit

---
+++ src/test/java/com/et/stadoorcli/StadoorCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.et.stadoorcli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StadoorCliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
